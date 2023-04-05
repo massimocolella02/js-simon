63a56372090b3859f4c4bed00b3e1c4e3689e496
massimocolella02 <massimo-colella@outlook.it>
2023-04-05 11:04:33 +0200

    Aggiornato

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,54 +1,44 @@
-const container= document.querySelector('.container');
-const button= document.querySelector('button').addEventListener('click', Invio);
-let span = document.createElement('p');
+//bottone
+document.querySelector('button').addEventListener('click', function(){
+    
+    //Creazione 5 numeri random
+    let arrayNumbers = []
+    for( let i=0; arrayNumbers.length < 5; i++){
+        let numero = randomNumbers();
+        if(!arrayNumbers.includes(numero)){
+            arrayNumbers.push(numero);
+            document.querySelector('.container').innerHTML+=`<p>${numero}</p>`;
+        }
+    }
 
-let arrayNumbers = [];
+    //Timer per far sparire i numeri
+    setTimeout(function(){
+        document.querySelector('.container').innerHTML='';
+    } , 10000)
 
-
+    //Timer per far comparire il prompt
+    let numeri_Utente = []
+    setTimeout( function(){
+        for(let k=0; numeri_Utente.length < 5; k++){
+            let numeroUtente =  parseInt(prompt('Inserisci i numeri che ricordi'));
+            if(!numeri_Utente.includes(numeroUtente)){
+                numeri_Utente.push(numeroUtente)
+            }
+        }
+        
+        //Verifica numeri
+        let numeri_Indovinati = []
+        for(let x= 0; x < 5; x++){
+            if(arrayNumbers.includes(numeri_Utente[x])){
+                numeri_Indovinati.push(numeri_Utente[x])
+            }
+        }
+        console.log(numeri_Indovinati, numeri_Indovinati.length)
+    }, 10100)
+    
+})
 
 function randomNumbers(){
-    let number= Math.floor(Math.random() * 1000) + 1;
-    arrayNumbers.push(number)
-    return number;
+    return Math.floor(Math.random() * 100) + 1
 }
 
-
-function Invio(){
-    //creazione numeri in pagina
-    container.innerHTML=''
-    let span = document.createElement('p');
-    for(let i = 0; i < 5; i++){
-        span = document.createElement('p');
-        span.innerHTML= randomNumbers();
-        container.appendChild(span);
-    }
-    //Rimozione elementi in pagina
-    setTimeout(function(){
-        container.innerHTML=''
-    }, 10*1000)
-    //Prompt per verifica numeri
-    setTimeout(function(){
-        let count = 0
-        for(let k=0; k <= 5; k++){
-            let promptValue = parseInt(prompt('Inserisci i numeri che ricordi'));
-            function verificaNumeri(){
-                if(arrayNumbers.includes(promptValue)){
-                    return promptValue;
-                }else{
-                    return false;
-                }
-            }
-            let result = verificaNumeri();
-            if( result == promptValue ){
-                console.log(promptValue)
-                count += 1
-            }
-            document.querySelector('.container').innerHTML=`Hai indovinato ${count} numeri!`
-        }
-    }, 11*1000)
-
-    
-    
-    console.log(arrayNumbers);
-    
-}
